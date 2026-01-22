070fe3664180d85ab6dc0dc03a567212684f2009
Shana <shanans264@gmail.com>
2026-01-22 14:51:14 +0800

    Add support for classes TodoTask, DeadlineTask and EventTask to Duchess

--- src/main/java/Duchess.java
+++ src/main/java/Duchess.java
@@ -11,27 +11,51 @@ public class Duchess {
         while (true) {
             String input = scanner.nextLine();
             String[] words = input.split(" ", 2);
-            if (input.equals("bye")) {
+            String command = words[0];
+
+            if (command.equals("bye")) {
                 System.out.println("Bye! See you again next time!");
                 break;
             }
-            if (input.equals("list")) {
+            if (command.equals("list")) {
                 System.out.println("Here is your todo list!");
                 todolist.print();
-            } else if (words[0].equals("mark")) {
+            } else if (command.equals("mark")) {
                 int taskNumber = Integer.parseInt(words[1]) - 1;
                 todolist.getTask(taskNumber).mark();
                 System.out.println("Congratulations on finishing your task! Task has been marked accordingly:\n" + todolist.getTask(taskNumber));
-            } else if (words[0].equals("unmark")) {
+            } else if (command.equals("unmark")) {
                 int taskNumber = Integer.parseInt(words[1]) - 1;
                 todolist.getTask(taskNumber).unmark();
                 System.out.println("Got it! Task has been unmarked accordingly:\n" + todolist.getTask(taskNumber));
-            } else {
-                Task task = new Task(input);
+            } else if (command.equals("todo")) {
+                Task task = new TodoTask(words[1]);
                 todolist.addTask(task);
-                System.out.println("added: " + input);
+                System.out.println("Sure! Task added:\n" + task);
+            } else if (command.equals("deadline")) {
+                String[] parts = words[1].split(" /by ", 2);
+                String description = parts[0];
+                String deadline = parts[1];
+
+                Task task = new DeadlineTask(description, deadline);
+                todolist.addTask(task);
+
+                System.out.println("Sure! Task added:\n" + task);
+            } else if (command.equals("event")) {
+                String[] first_parts = words[1].split(" /from ", 2);
+                String[] second_parts = first_parts[1].split(" /to ", 2);
+                String description = first_parts[0];
+                String start = second_parts[0];
+                String end = second_parts[1];
+
+                Task task = new EventTask(description, start, end);
+                todolist.addTask(task);
+
+                System.out.println("Sure! Task added:\n" + task);
+            } else {
+                    System.out.println("Sorry!");
+                }
             }
-        }
 
         scanner.close();
     }
