425d3b3f8a0957ad5ee23261a48562f61e344ee8
Shana <shanans264@gmail.com>
2026-01-22 08:35:25 +0800

    Rename chatbot to Duchess and add greeting and exit

---
+++ src/main/java/Duchess.java
@@ -0,0 +1,7 @@
+public class Duchess {
+    public static void main(String[] args) {
+        System.out.println("Good day to you! I'm Duchess.");
+        System.out.println("What can I do for you today?");
+        System.out.println("Bye! See you again next time!");
+    }
+}
