ed0acb518e41a633d83b12d85a68b3ee567a64e8
Shana <shanans264@gmail.com>
2026-01-22 14:09:24 +0800

    Add class EventTask to represent an Event task

---
+++ src/main/java/EventTask.java
@@ -0,0 +1,15 @@
+public class EventTask extends Task {
+    private final String start;
+    private final String end;
+
+    public EventTask(String description, String start, String end) {
+        super(description);
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + this.start + " to: " + this.end + ")";
+    }
+}
