3d529782f353ba3bc42fab456e26108b3f55d7f4
Shana <shanans264@gmail.com>
2026-01-22 10:42:37 +0800

    Add support for class Task in TodoList

--- src/main/java/TodoList.java
+++ src/main/java/TodoList.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
  * Stores a list of tasks for the user to complete.
  */
 public class TodoList {
-    private final ArrayList<String> tasks;
+    private final ArrayList<Task> tasks;
 
     public TodoList() {
         this.tasks = new ArrayList<>();
     }
 
-    public void addTask(String task) {
+    public void addTask(Task task) {
         this.tasks.add(task);
     }
 
     public void print() {
         for (int i = 0; i < this.tasks.size(); i++) {
-            System.out.println((i + 1) + ". " + this.tasks.get(i));
+            System.out.println((i + 1) + ". " + this.tasks.get(i).toString());
         }
     }
 }
