ce075c857452865939e1a61e75df78ed3b960fa9
Shana <shanans264@gmail.com>
2026-01-22 09:02:37 +0800

    Add support for echo and exit on bye

--- src/main/java/Duchess.java
+++ src/main/java/Duchess.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Duchess {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Good day to you! I'm Duchess.");
         System.out.println("What can I do for you today?");
-        System.out.println("Bye! See you again next time!");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye! See you again next time!");
+                break;
+            }
+            System.out.println(input);
+        }
     }
 }
