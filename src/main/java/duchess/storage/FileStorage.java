187700b94ad6a3aafb1b024a0f9de5d7e6cd5cc5
Shana <shanans264@gmail.com>
2026-01-31 09:36:22 +0800

    Change code to fit coding standard

--- src/main/java/duchess/storage/FileStorage.java
+++ src/main/java/duchess/storage/FileStorage.java
@@ -5,11 +5,12 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+
 import duchess.list.TodoList;
-import duchess.task.Task;
-import duchess.task.TodoTask;
 import duchess.task.DeadlineTask;
 import duchess.task.EventTask;
+import duchess.task.Task;
+import duchess.task.TodoTask;
 
 /**
  * Handles writing tasks to and fetching tasks from the hard disk.
